b701122a9a44197baabc104006b7b09b2fac10d9
Ronald Dahlgren <ronald.dahlgren@stansonhealth.com>
2017-04-14 00:12:42 -0400

    Implement BinaryTree merge and delete

--- lib/BinaryTree.ex
+++ lib/BinaryTree.ex
@@ -19,10 +19,10 @@ defmodule BinaryTree do
   def is_member?(value, %BinaryTree{item: value}), do: true
 
   def is_member?(value, %BinaryTree{item: item}=set) when value < item do
-    member(value, set.left)
+    is_member?(value, set.left)
   end
   def is_member?(value, %BinaryTree{item: item}=set) when value > item do
-    member(value, set.right)
+    is_member?(value, set.right)
   end
 
   def insert(value, set \\ @terminal)
@@ -49,11 +49,67 @@ defmodule BinaryTree do
     }
   end
 
+  def merge(@terminal, @terminal), do: @terminal
+  def merge(%BinaryTree{}=x, @terminal), do: x
+  def merge(@terminal, %BinaryTree{}=y), do: y
+  def merge(%BinaryTree{item: xvalue}=x, %BinaryTree{item: yvalue}=y) when xvalue < yvalue do
+    %BinaryTree{
+      left: merge(x, y.left),
+      item: yvalue,
+      right: y.right
+    }
+  end
+  def merge(%BinaryTree{item: xvalue}=x, %BinaryTree{item: yvalue}=y) when xvalue > yvalue do
+    %BinaryTree{
+      left: y.left,
+      item: yvalue,
+      right: merge(x, y.right)
+    }
+  end
+  def merge(%BinaryTree{item: value}=x, %BinaryTree{item: value}=y) do
+    %BinaryTree{
+      left: merge(x.left, y.left),
+      item: value,
+      right: merge(x.right, y.right)
+    }
+  end
+
+  def delete(_, @terminal), do: @terminal
+  def delete(_, %BinaryTree{left: @terminal, right: @terminal}), do: @terminal
+  def delete(value, %BinaryTree{left: @terminal, item: value, right: right}), do: right
+  def delete(value, %BinaryTree{left: left, item: value, right: @terminal}), do: left
+  def delete(value, %BinaryTree{left: %BinaryTree{}=left, item: value, right: %BinaryTree{}=right}) do
+    %BinaryTree{
+      left: merge(left, right.left),
+      item: right.item,
+      right: right.right
+    }
+  end
+
+  def delete(value, %BinaryTree{item: item}=set) when value < item do
+    %BinaryTree{
+      left: delete(value, set.left),
+      item: set.item,
+      right: set.right
+    }
+  end
+
+  def delete(value, %BinaryTree{item: item}=set) when value > item do
+    %BinaryTree{
+      left: set.left,
+      item: set.item,
+      right: delete(value, set.right)
+    }
+  end
+
   def go() do
     s = 1..10
         |> Enum.shuffle
         |> Enum.reduce(@terminal, &BinaryTree.insert/2)
     IO.puts "Built a set from 1..10 shuffled:"
     IO.inspect s
+
+    IO.puts "Deleting 3"
+    IO.inspect delete(3, s)
   end
 end
