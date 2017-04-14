3fb13f774b31a82e4654af5fdbf5b1380fb08d86
Ronald Dahlgren <ronald.dahlgren@stansonhealth.com>
2017-04-14 00:45:25 -0400

    Avoid unnecessary copying

--- lib/BinaryTree.ex
+++ lib/BinaryTree.ex
@@ -8,6 +8,14 @@ defmodule BinaryTree do
   This implementation behaves as a Set
   """
 
+  defmodule DuplicateValueException do
+    defexception message: "value exists"
+  end
+
+  defmodule MissingValueException do
+    defexception message: "value exists"
+  end
+
   defstruct [:left, :item, :right]
 
   @terminal %{left: nil, item: nil, right: nil}
@@ -16,38 +24,44 @@ defmodule BinaryTree do
   def is_empty?(_), do: false
 
   def is_member?(_, @terminal), do: false
-  def is_member?(value, %BinaryTree{item: value}), do: true
-
   def is_member?(value, %BinaryTree{item: item}=set) when value < item do
     is_member?(value, set.left)
   end
   def is_member?(value, %BinaryTree{item: item}=set) when value > item do
     is_member?(value, set.right)
   end
+  def is_member?(_, _), do: true
 
-  def insert(value, set \\ @terminal)
-  def insert(value, @terminal) do
+  def insert(value, set) do
+    try do
+      _insert(value, set)
+    rescue
+      DuplicateValueException -> set
+    end
+  end
+
+  defp _insert(value, @terminal) do
     %BinaryTree{
       left: @terminal,
       item: value,
       right: @terminal
     }
   end
-  def insert(value, %BinaryTree{item: value}=set), do: set
-  def insert(value, %BinaryTree{item: item}=set) when value < item do
+  defp _insert(value, %BinaryTree{item: item}=set) when value < item do
     %BinaryTree{
-      left: insert(value, set.left),
+      left: _insert(value, set.left),
       item: set.item,
       right: set.right
     }
   end
-  def insert(value, %BinaryTree{item: item}=set) when value > item do
+  defp _insert(value, %BinaryTree{item: item}=set) when value > item do
     %BinaryTree{
       left: set.left,
       item: set.item,
-      right: insert(value, set.right)
+      right: _insert(value, set.right)
     }
   end
+  defp _insert(_, _), do: raise DuplicateValueException
 
   def merge(@terminal, @terminal), do: @terminal
   def merge(%BinaryTree{}=x, @terminal), do: x
@@ -74,11 +88,35 @@ defmodule BinaryTree do
     }
   end
 
-  def delete(_, @terminal), do: @terminal
-  def delete(_, %BinaryTree{left: @terminal, right: @terminal}), do: @terminal
-  def delete(value, %BinaryTree{left: @terminal, item: value, right: right}), do: right
-  def delete(value, %BinaryTree{left: left, item: value, right: @terminal}), do: left
-  def delete(value, %BinaryTree{left: %BinaryTree{}=left, item: value, right: %BinaryTree{}=right}) do
+  def delete(value, set) do
+    try do
+      _delete(value, set)
+    rescue
+      MissingValueException -> set
+    end
+  end
+
+  defp _delete(_, @terminal), do: raise MissingValueException
+  defp _delete(_, %BinaryTree{left: @terminal, right: @terminal}), do: @terminal
+  defp _delete(value, %BinaryTree{left: @terminal, item: value, right: right}), do: right
+  defp _delete(value, %BinaryTree{left: left, item: value, right: @terminal}), do: left
+  defp _delete(value, %BinaryTree{item: item}=set) when value < item do
+    %BinaryTree{
+      left: _delete(value, set.left),
+      item: set.item,
+      right: set.right
+    }
+  end
+
+  defp _delete(value, %BinaryTree{item: item}=set) when value > item do
+    %BinaryTree{
+      left: set.left,
+      item: set.item,
+      right: _delete(value, set.right)
+    }
+  end
+
+  defp _delete(_, %BinaryTree{left: %BinaryTree{}=left, right: %BinaryTree{}=right}) do
     %BinaryTree{
       left: merge(left, right.left),
       item: right.item,
@@ -86,21 +124,6 @@ defmodule BinaryTree do
     }
   end
 
-  def delete(value, %BinaryTree{item: item}=set) when value < item do
-    %BinaryTree{
-      left: delete(value, set.left),
-      item: set.item,
-      right: set.right
-    }
-  end
-
-  def delete(value, %BinaryTree{item: item}=set) when value > item do
-    %BinaryTree{
-      left: set.left,
-      item: set.item,
-      right: delete(value, set.right)
-    }
-  end
 
   def go() do
     s = 1..10
@@ -108,6 +131,12 @@ defmodule BinaryTree do
         |> Enum.reduce(@terminal, &BinaryTree.insert/2)
     IO.puts "Built a set from 1..10 shuffled:"
     IO.inspect s
+
+    IO.puts "Inserting another shuffled 1..10, expecting no change:"
+    s2 = 1..10
+      |> Enum.shuffle
+      |> Enum.reduce(s, &BinaryTree.insert/2)
+    IO.inspect s2
 
     IO.puts "Deleting 3"
     IO.inspect delete(3, s)
