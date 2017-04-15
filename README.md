642845b09a88cd8ef4318adbcfec91ac17d14e37
Ronald Dahlgren <ronald.dahlgren@stansonhealth.com>
2017-04-14 20:38:48 -0400

    Make an escript wrapper

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ This repository is where I collect scratch modules used while working through
 Chris Okasaki's [Purely Functional Data Structures](https://www.amazon.com/Purely-Functional-Structures-Chris-Okasaki/dp/0521663504)
 
 
+## For the Wolfies
+
+Generate some names with:
+
+        mix escript.build
+        ./pfds --firstnames /path/to/firstnames.txt --surnames /path/to/surnames.txt --count 100
+
--- lib/pfds.ex
+++ lib/pfds.ex
@@ -1,6 +1,33 @@
 defmodule Pfds do
-  def go() do
-    generate_alias(20, "../cheeses.txt", "../surnames.txt")
+  @default_count 10
+
+  def main(args) do
+    { opts, _, _} = OptionParser.parse(args,
+           switches: [firstnames: :string, surnames: :string, count: :integer])
+
+    first_names = opts[:firstnames]
+    surnames = opts[:surnames]
+    if first_names == nil or surnames == nil do
+      print_usage()
+      exit {:shutdown, 1}
+    end
+
+    count = case opts[:count] do
+      nil -> @default_count
+      x -> x
+    end
+    generate_alias(count, first_names, surnames)
+  end
+
+  defp print_usage() do
+    IO.puts """
+    pfds --firstnames FIRSTNAMES_FILE --surnames SURNAMES_FILE [--count COUNT]
+
+    Args:
+      FIRSTNAMES_FILE Path to a file containing newline separated first names
+      SURNAMES_FILE Path to a file containing newline separated surnames
+      COUNT optionally specify how many samples to pull. Default: #{@default_count}
+    """
   end
 
   def generate_alias(count, given_names_file, surnames_file) do
